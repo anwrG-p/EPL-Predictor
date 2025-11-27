e79d71ddd95b5b75e2ce1ddb1e074830acfe86b6
Anwer <anouarguemri1@gmail.com>
2025-11-27 23:20:02 +0100

    ts ain't srs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {HashRouter as Router, BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Predictor from './components/Predictor';
 import AdminPanel from './components/AdminPanel';
